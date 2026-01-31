98654cb7012c65902a44119af34067d0b0657e63
mbubere23cs-ux <mbubere23cs@student.mes.ac.in>
2026-01-31 15:20:46 +0530

    Create routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+router.get("/", function(req, res){
+	res.render("landing");
+});
+
+//===========
+//Auth Routes
+//===========
+
+//show registration form
+router.get("/register", function(req,res){
+	res.render("register");
+});
+
+//handle signup logic
+router.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username, name: req.body.name, email: req.body.email});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			req.flash("error", err.message);
+			return res.redirect("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			req.flash("success", "Welcome to YelpCamp " + user.username);
+			res.redirect("/campgrounds");
+		});
+	});
+});
+
+//show login form
+router.get("/login", function(req, res){
+	res.render("login");
+});
+
+router.post("/login", passport.authenticate("local",
+	{
+		successRedirect: "/campgrounds",
+		failureRedirect: "/login"
+	}), function(req, res){	
+});
+
+//logout route
+router.get("/logout", function(req, res){
+	req.logout();
+	req.flash("success", "You have been logged out.");
+	res.redirect("/campgrounds");
+});
+
+module.exports = router;
