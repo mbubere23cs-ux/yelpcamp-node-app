a728078587b98978293a160b6cfc62ece7e3abf6
mbubere23cs-ux <mbubere23cs@student.mes.ac.in>
2026-01-31 15:27:00 +0530

    Create models/campground.js

---
+++ models/campground.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	price: String,
+	image: String,
+	description: String,
+	author:{
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String		
+	},
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Comment"
+		}
+	]
+});
+
+//Creation of db model
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+module.exports = Campground;
